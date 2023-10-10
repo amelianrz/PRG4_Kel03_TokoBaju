903bf03ac11c99263efbc18caf556556b96fde76
Lenovo <nurzahidahamelia241@gmail.com>
2023-10-10 13:48:22 +0700

    Add project files.

---
+++ PRG4_Kel03_TokoBaju/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRG4_Kel03_TokoBaju.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
